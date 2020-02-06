0a6490f5c5baa0f41276046063c47969e85f38ad
hoangviethung <hoangviet.hung.009@gmail.com>
2020-02-06 22:58:41 +0700

    xong component hoạt động (trang chủ)

--- src/app/components/contact/contact.module.ts
+++ src/app/components/contact/contact.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { MapComponent } from '../_shared/map/map.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
 	declarations: [
-		ContactComponent
+		ContactComponent,
+		MapComponent
 	],
 	imports: [
 		CommonModule,
 		ContactRoutingModule,
-		TranslateModule.forChild()
+		TranslateModule.forChild(),
+		AgmCoreModule.forRoot({
+			apiKey: "AIzaSyAq6ag0zUMblc-Wk5Z5S8KWkHpT3ak1LzI",
+			libraries: ["places", "geometry"]
+		})
 	]
 })
 export class ContactModule { }
