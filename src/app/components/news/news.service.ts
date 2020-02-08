40a612575a201147d75c292a5988953143890238
sonvh2510 <sonvh2510@gmail.com>
2020-02-08 16:29:34 +0700

    update code to deploy heroku

--- src/app/components/news/news.service.ts
+++ src/app/components/news/news.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from 'src/app/shared/services/http.service';
 import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 
@@ -10,14 +11,15 @@ export class NewsService {
 
 	constructor(
 		private httpSvc: HttpService,
+		private http: HttpClient
 	) {
 	}
 
-	fetchNews(url: string) {
-		return this.httpSvc.get(url);
+	fetchNews(url: string): Observable<any> {
+		return this.http.get(url);
 	}
 
-	fetchNewsDetail(url:string) {
-		return this.httpSvc.get(url);
+	fetchNewsDetail(url: string): Observable<any> {
+		return this.http.get(url);
 	}
 }
