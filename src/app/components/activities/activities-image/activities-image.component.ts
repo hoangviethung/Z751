a33dbbd3baa4546719370bce0685a6ab90d9824c
sonvh2510 <sonvh2510@gmail.com>
2020-02-09 18:53:45 +0700

    update sync swiper slider

--- src/app/components/activities/activities-image/activities-image.component.ts
+++ src/app/components/activities/activities-image/activities-image.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpService } from 'src/app/shared/services/http.service';
 import { PageInfoService } from 'src/app/shared/services/page-info.service';
 import { TranslateService } from '@ngx-translate/core';
+import { SwiperConfigInterface, SwiperDirective } from 'ngx-swiper-wrapper';
 
 @Component({
 	selector: 'app-activities-image',
@@ -12,20 +13,73 @@ export class ActivitiesImageComponent implements OnInit {
 	imageUrl = 'assets/db/vi/activities-images.json';
 	images = [];
 
+	@ViewChild(SwiperDirective, { static: false }) smallSlider: SwiperDirective;
+	@ViewChild(SwiperDirective, { static: false }) bigSlider: SwiperDirective;
+
+	smallSliderConfig: SwiperConfigInterface = {
+		slidesPerView: 5,
+		spaceBetween: 55,
+		loop: true,
+		observer: true,
+		observeParents: true,
+		navigation: {
+			nextEl: '.slider-reward .swiper-button-next',
+			prevEl: '.slider-reward .swiper-button-prev',
+		},
+		breakpoints: {
+			1025: {
+				spaceBetween: 30,
+			},
+			768: {
+				spaceBetween: 15,
+				slidesPerView: 2,
+			}
+		}
+	}
+	bigSliderConfig: SwiperConfigInterface = {
+		slidesPerView: 1,
+		speed: 500,
+		spaceBetween: 55,
+		observer: true,
+		observeParents: true,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true,
+		},
+		navigation: {
+			nextEl: '.slider-reward .swiper-button-next',
+			prevEl: '.slider-reward .swiper-button-prev',
+		},
+		breakpoints: {
+			1025: {
+				spaceBetween: 30,
+			},
+			768: {
+				spaceBetween: 15,
+				slidesPerView: 2,
+			}
+		},
+	}
+
 	constructor(
 		private httpSvc: HttpService,
 		private pageInfoSvc: PageInfoService,
 		private translateSvc: TranslateService
 	) { }
 
+	getImages() {
+		this.httpSvc.get(this.imageUrl).subscribe(result => {
+			this.images = result.data;
+		});
+	}
+
 	ngOnInit() {
 		this.pageInfoSvc.setTitle(this.translateSvc.instant('menu.images'));
 		this.getImages();
 	}
 
-	getImages() {
-		this.httpSvc.get(this.imageUrl).subscribe(result => {
-			this.images = result.data;
-		});
+	changeBigSlider(e) {
+		const clickedIndex = Number((<HTMLElement>e.target).getAttribute('data-swiper-slide-index'));
+		this.bigSlider.setIndex(clickedIndex);
 	}
 }
--- src/app/components/activities/activities.module.ts
+++ src/app/components/activities/activities.module.ts
@@ -8,7 +8,12 @@ import { ActivitiesVideoComponent } from './activities-video/activities-video.co
 import { ActivitiesPopupComponent } from './activities-popup/activities-popup.component';
 import { ImageSimpleComponent } from './activities-image/image-simple/image-simple.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SwiperConfigInterface, SwiperModule, SWIPER_CONFIG } from 'ngx-swiper-wrapper';
 
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+	direction: 'horizontal',
+	slidesPerView: 'auto'
+};
 
 @NgModule({
 	declarations: [
@@ -20,8 +25,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
 	imports: [
 		CommonModule,
 		SharedModule,
+		SwiperModule,
 		ActivitiesRoutingModule,
 		TranslateModule.forChild()
+	],
+	providers: [
+		{
+			provide: SWIPER_CONFIG,
+			useValue: DEFAULT_SWIPER_CONFIG
+		}
 	]
 })
 export class ActivitiesModule { }
