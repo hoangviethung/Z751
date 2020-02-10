873bf3eaa94b08194e528bc7af84eb09fae64250
sonvh2510 <sonvh2510@gmail.com>
2020-02-10 11:07:52 +0700

    update

--- src/app/shared/directives/close-menu.directive.ts
+++ src/app/shared/directives/close-menu.directive.ts
@@ -12,8 +12,9 @@ export class CloseMenuDirective implements OnInit {
 	ngOnInit() {
 		const anchor = <HTMLElement>this.elemenRef.nativeElement;
 		const header = document.querySelector('header');
-		const overlay = header.querySelector('.overlay');
 		const body = document.querySelector('body');
+		const overlay = header.querySelector('.overlay');
+		const navSubs = header.querySelectorAll('.nav-sub');
 
 		const getNavbar = (element) => {
 			if (Array.from(element.classList).includes('navbar')) {
@@ -22,10 +23,14 @@ export class CloseMenuDirective implements OnInit {
 				return getNavbar(element.parentNode);
 			}
 		}
+
 		anchor.addEventListener('click', () => {
 			getNavbar(anchor).classList.remove('active');
 			overlay.classList.remove('active');
 			body.removeAttribute('style');
+			navSubs.forEach(sub => {
+				sub.classList.remove('active');
+			})
 		})
 	}
 }
