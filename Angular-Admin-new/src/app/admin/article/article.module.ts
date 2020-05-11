8bbb5c5788990fb044e4579309a417894b1d7770
sonvh2510 <sonvh2510@gmail.com>
2020-05-11 17:55:00 +0700

    update thêm sửa sản phẩm

--- Angular-Admin-new/src/app/admin/article/article.module.ts
+++ Angular-Admin-new/src/app/admin/article/article.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ArticleRoutingModule } from './article-routing.module';
 import { ArticleComponent } from './article.component';
 import { AddEditComponent } from './add-edit/add-edit.component';
 import { SharedModule } from 'src/app/_core/shared.module';
 
-
 @NgModule({
 	declarations: [ArticleComponent, AddEditComponent],
-	imports: [
-		CommonModule,
-		ArticleRoutingModule,
-		SharedModule
-	]
+	imports: [ArticleRoutingModule, SharedModule],
 })
-export class ArticleModule { }
+export class ArticleModule {}
