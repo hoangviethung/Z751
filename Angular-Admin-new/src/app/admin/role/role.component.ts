963b83af7caa588e2ad54d757d3985f7a0429e2c
Hoang Nguyen <hoang@magetechcorp.com>
2020-05-28 18:13:29 +0700

    Add some features
    + Add manage menu by permission,
    + Add canAction function for checking permission of this action.
    + Apply canAction into all page that manage the data.

--- Angular-Admin-new/src/app/admin/role/role.component.ts
+++ Angular-Admin-new/src/app/admin/role/role.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
 import { RoleModel } from 'src/app/_core/models/role.model';
 import { APIConfig } from 'src/app/_core/API-config';
 import { map } from 'rxjs/operators';
@@ -6,20 +6,27 @@ import { Router } from '@angular/router';
 import { CrudService } from 'src/app/_core/services/crud.service';
 import { InputRequestOption } from 'src/app/_core/services/http.service';
 import { ToastrService } from 'ngx-toastr';
+import {AuthenticationComponent} from "../../_core/components/base/authentication.component";
+import {Permissions} from "../../_core/enums/role.enum";
 
 @Component({
 	selector: 'app-role',
 	templateUrl: './role.component.html',
 	styleUrls: ['./role.component.scss'],
 })
-export class RoleComponent implements OnInit {
+export class RoleComponent extends AuthenticationComponent implements OnInit {
+	public featureName: string = 'ManagePermissions';
+	public permissions = Permissions;
+
 	roles: Array<RoleModel>;
 
 	constructor(
+		injector: Injector,
 		private crudSvc: CrudService,
-		private router: Router,
 		private toastrSvc: ToastrService
-	) { }
+	) {
+		super(injector);
+	}
 
 	ngOnInit(): void {
 		this.fetchRole();
